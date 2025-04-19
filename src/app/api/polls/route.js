eae86bdd8f9426fbc2943e92abdf36f7281b10ce
Franco Androetto <androettofranco@gmail.com>
2025-04-19 10:29:46 -0300

    🛠️ build: change file

--- src/app/api/polls/route.js
+++ src/app/api/polls/route.js
@@ -1,7 +1,7 @@
 export const runtime = 'edge';
 
-import connect from '../../../lib/mongodb'
-import Poll from '../../../models/Poll'
+import connect from '@lib/mongodb'
+import Poll from '@models/Poll2'
 
 export async function POST(req) {
   try {
