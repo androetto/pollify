804c261ee80df8adc6b14e8984652d67a1ecaaa4
Franco Androetto <androettofranco@gmail.com>
2025-04-19 10:22:24 -0300

    🛠️ build: add export edge

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // runtime: 'edge', // Removed as it is not a valid property
+  },
 };
 
 export default nextConfig;
--- src/app/api/polls/route.js
+++ src/app/api/polls/route.js
@@ -1,4 +1,4 @@
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 import connect from '../../../lib/mongodb'
 import Poll from '../../../models/Poll'
